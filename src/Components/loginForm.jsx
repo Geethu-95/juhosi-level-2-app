568f5e4cb495a33ad081ec7108b9608e5a3353e0
Geethika S <geethu.s1995@gmail.com>
2023-07-02 18:07:02 +0530

    Updated to add two .then in loginForm

--- src/Components/loginForm.jsx
+++ src/Components/loginForm.jsx
@@ -60,12 +60,12 @@ export default function LoginForm() {
       'content-type': 'application/json'
   }}
       )
-      // .then((response) => response.json())
+      .then((response) => response.json())
       .then((data) => {
         if (data) {
           console.log("data ",data);
           navigate("/orderEntry", {
-            state: { owner: data[data].data[0].name, id: data[data].data[0].id },
+            state: { owner: data[0].name, id: data[0].id },
           });
         } else alert("Invalid credentials!");
       });
